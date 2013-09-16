1577b7ba5428ed7deebb1cb94db0de521a89fdfe
William Notowidagdo <wnotowidagdo@gmail.com>
2013-09-16 18:13:48 +0700

    Added placeholder modal dialog for Delete application

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -5,7 +5,33 @@
     <div class="page-header"><h1>List of applications <a href="/add-application" class="btn btn-success">Create new</a></h1></div>
     <div class="row">
         <div class="col-lg-6">
-            <h3>Hello world <a href="/edit-application" class="btn btn-primary">Edit</a></h3>
+            <h3>
+                Hello world
+                <a href="/edit-application" class="btn btn-primary">Edit</a>
+                <a href="#deleteApplication" class="btn btn-danger btn-xs" data-toggle="modal">
+                    Delete
+                </a>
+            </h3>
+
+            <!-- Modal -->
+            <div class="modal fade" id="deleteApplication" tabindex="-1" role="dialog" aria-labelledby="deleteApplicationLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Delete application</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Are you sure you want to delete application "Hello world"?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-danger">Delete application</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <p>
                 Key: <code>8au1s7rs2r3</code>
             </p>
