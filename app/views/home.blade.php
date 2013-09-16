28a340c1722e25fa22d25ea31bfb85814f68ab03
William Notowidagdo <wnotowidagdo@gmail.com>
2013-09-16 13:01:27 +0700

    Added master layout

---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+<h1>PemiluAPI developer website</h1>
+@stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PemiluAPI developer website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
