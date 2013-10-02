dc77c8b3cf3b922787aeb716e121b3412e1e6d77
William Notowidagdo <wnotowidagdo@gmail.com>
2013-10-02 10:38:42 +0700

    Added Endpoint description

--- app/views/endpoints.blade.php
+++ app/views/endpoints.blade.php
@@ -5,6 +5,11 @@
     <div class="page-header"><h1>List of API endpoints</h1></div>
     <div class="row">
         <div class="col-lg-6">
+            <p class="lead">
+                Endpoint, the entry point to a service, a process,
+                or a queue or topic destination in service-oriented architecture.
+            </p>
+
             @foreach ($endpoints as $endpoint)
             <h3>{{ $endpoint['name'] }}</h3>
             <p>{{ $endpoint['desc'] }}</p>
