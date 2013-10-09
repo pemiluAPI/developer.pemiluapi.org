d827f2149bdc8922e8be41cb6217ee3c79df3490
William Notowidagdo <wnotowidagdo@gmail.com>
2013-10-09 08:31:03 +0700

    Added API documentation link

--- app/views/documentation.blade.php
+++ app/views/documentation.blade.php
@@ -6,12 +6,8 @@
     <div class="row">
         <div class="col-lg-8">
             <p>
-                Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus
-                ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
-                justo sit amet risus. Sed posuere consectetur est at lobortis. Vivamus
-                sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed
-                odio dui. Maecenas faucibus mollis interdum. Etiam porta sem malesuada
-                magna mollis euismod.
+                Temukan dokumentasi terbaru PemiluAPI di
+                {{ link_to('http://docs.pemilunews.apiary.io/', 'docs.pemilunews.apiary.io', array('target' => '_blank')) }}
             </p>
         </div>
     </div>
