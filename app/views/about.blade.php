630b9fc05819454eb63fb2ea344f65063e9b775f
Thomas Apodaca <tma@angrymonkey.com>
2014-03-26 11:30:48 -0700

    changed kunci to key

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -19,9 +19,9 @@
             <p class="lead">Selamat datang di situs khusus pengembang API Pemilu, API informasi sipil untuk Pemilu 2014 dan seterusnya!</p>
             <p>Jika kamu ingin membangun sebuah aplikasi dengan menggunakan API ini, kamu telah datang ke tempat yang tepat.</p>
 
-            <p>Silahkan mendaftar untuk mendapatkan kunci API dengan membuat akun dan mendaftarkan aplikasi, atau gunakan kunci gratis ini: <strong>fea6f7d9ec0b31e256a673114792cb17</strong></p>
+            <p>Silahkan mendaftar untuk mendapatkan key API dengan membuat akun dan mendaftarkan aplikasi, atau gunakan key gratis ini: <strong>fea6f7d9ec0b31e256a673114792cb17</strong></p>
 
-            <p>Kami sarankan agar anda mendaftarkan kunci anda sendiri apabila akan menggunakan API ini untuk aplikasi produksi.</p>
+            <p>Kami sarankan agar anda mendaftarkan key anda sendiri apabila akan menggunakan API ini untuk aplikasi produksi.</p>
 
             <p>Dokumentasi untuk endpoint API dapat dilihat di sini: http://developer.pemiluapi.org/endpoints</p>
 
